7997347d8369f9021c5de5ed56a1f5e78bd7608e
BeiNing-Z <2233802521@qq.com>
2024-12-18 19:57:30 +0800

    final

--- exercises/33_std_accumulate/main.cpp
+++ exercises/33_std_accumulate/main.cpp
@@ -12,6 +12,10 @@ int main(int argc, char **argv) {
     //       - 连续存储；
     //       的张量占用的字节数
     // int size =
+    // 使用 std::accumulate 计算形状的总元素数
+    int num_elements = std::accumulate(shape, shape + 4, 1, std::multiplies<int>());
+    // 计算占用的字节数
+    int size = num_elements * sizeof(DataType);
     ASSERT(size == 602112, "4x1x3x224x224 = 602112");
     return 0;
 }
